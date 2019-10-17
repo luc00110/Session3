8a6718e474ffab89b5ad9c1c16402b031c951e6e
LucienBR <me@lucienbr.com>
2019-10-17 12:31:12 -0400

    MOVED ASSIGNMENTS TO PRIVATE REPO

--- IFT287/affichage.sql
+++
@@ -1,7 +0,0 @@
-
-SELECT * FROM Membre;
-SELECT * FROM Lot;
-SELECT * FROM LotMembre;
-SELECT * FROM DemandeMembreLot;
-SELECT * FROM Plante;
-SELECT * FROM Culture;
--- IFT287/destruction.sql
+++
@@ -1,6 +0,0 @@
-DROP TABLE Culture;
-DROP TABLE LotMembre;
-DROP TABLE DemandeMembreLot;
-DROP TABLE Membre;
-DROP TABLE Lot;
-DROP TABLE Plante;
